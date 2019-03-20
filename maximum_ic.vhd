daa4bb67dccd041f89a32de9860828a12dd5e1d7
Moamen Hassan Attia <MoamenAttia@outlook.com>
2019-03-20 05:57:36 +0200

    Comparator IC 100% Done using primitive gates

--- maximum_ic.vhd
+++ maximum_ic.vhd
@@ -25,10 +25,65 @@ architecture a_maximum_ic of maximum_ic is
   
   type state_type is ( idle , state_1 , state_2 , state_3 , state_4 , state_5, state_6 , state_7 , state_8 , state_9 , state_10 );
   signal state : state_type := idle; 
+
+  signal alu_inp1  : std_logic_vector(15 downto 0) := (others => '0'); -- a
+  signal alu_inp2	 : std_logic_vector(15 downto 0) := (others => '0'); -- b
+  signal alu_out   : std_logic_vector(15 downto 0) := (others => '0'); -- add or sub
+  signal alu_sel   : std_logic := '0';
+  signal alu_cin   : std_logic := '0';
+  signal alu_cout  : std_logic := '0';
+
   begin
     process(clk)
     begin
       if(rising_edge(clk)) then
+        if (start_comp = '0' and not(state = idle)) then
+          case state is
+            when state_1 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label1_out;
+              alu_sel <= '1';
+            when state_2 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label2_out;
+              alu_sel <= '1';
+            when state_3 =>  
+              alu_inp1 <= answer;
+              alu_inp2 <= label3_out;
+              alu_sel <= '1';
+            when state_4 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label4_out;
+              alu_sel <= '1';   
+            when state_5 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label5_out;
+              alu_sel <= '1';
+            when state_6 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label6_out;
+              alu_sel <= '1';
+            when state_7 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label7_out;
+              alu_sel <= '1';
+            when state_8 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label8_out;
+              alu_sel <= '1';
+            when state_9 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label9_out;
+              alu_sel <= '1';
+            when state_10 =>
+              alu_inp1 <= answer;
+              alu_inp2 <= label10_out;
+              alu_sel <= '1';
+            when others =>
+                state <= idle;
+          end case;
+        end if;
+      elsif falling_edge(clk) then
         if start_comp = '1' then
           start_comp <= '0'; state <= state_1;
           done_comp <= '0';
@@ -36,60 +91,61 @@ architecture a_maximum_ic of maximum_ic is
         elsif (start_comp = '0' and not(state = idle)) then
           case state is
             when state_1 =>
-              if to_integer(signed(answer)) < to_integer(signed(label1_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label1_out;
-              end if ;
+              end if;
               state <= state_2;
             when state_2 =>
-              if to_integer(signed(answer)) < to_integer(signed(label2_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label2_out;
-              end if ;
+              end if;
               state <= state_3;
             when state_3 =>  
-              if to_integer(signed(answer)) < to_integer(signed(label3_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label3_out;
-              end if ;
-              state <= state_4;
+              end if;
+              state <= state_4;       
             when state_4 =>
-              if to_integer(signed(answer)) < to_integer(signed(label4_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label4_out;
-              end if ;
-              state <= state_5;
+              end if;
+              state <= state_5;   
             when state_5 =>
-              if to_integer(signed(answer)) < to_integer(signed(label5_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label5_out;
-              end if ;
+              end if;
               state <= state_6;
             when state_6 =>
-              if to_integer(signed(answer)) < to_integer(signed(label6_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label6_out;
-              end if ;
+              end if;
               state <= state_7;
             when state_7 =>
-              if to_integer(signed(answer)) < to_integer(signed(label7_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label7_out;
-              end if ;
+              end if;
               state <= state_8;
             when state_8 =>
-              if to_integer(signed(answer)) < to_integer(signed(label8_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label8_out;
-              end if ;
+              end if;
               state <= state_9;
             when state_9 =>
-              if to_integer(signed(answer)) < to_integer(signed(label9_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label9_out;
-              end if ;
+              end if;
               state <= state_10;
             when state_10 =>
-              if to_integer(signed(answer)) < to_integer(signed(label10_out)) then
+              if(alu_out(15) = '1') then
                 answer <= label10_out;
-              end if ;
+              end if;
               state <= idle;
               done_comp <= '1';
             when others =>
                 state <= idle;
           end case;
-        end if;
+        end if;   
       end if ;
     end process;
+    alu_subtractor_adder : entity work.alu generic map ( 16 ) port map ( alu_inp1 , alu_inp2 , alu_sel , alu_cin , alu_out , alu_cout );
 end a_maximum_ic;
